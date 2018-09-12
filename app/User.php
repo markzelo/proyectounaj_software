1201f4ec37017aeb6394dafe29453c41d09eabf3
DESKTOP-O1A3RUG\markzelo11 <markzzelo28@gmail.com>
2018-09-12 20:35:54 -0300

    modelos correciones en orm

--- app/User.php
+++ app/User.php
@@ -28,4 +28,74 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //selector de tipo deusuario
+
+
+    public function getIsAdminAttribute()
+    {
+        return $this->role == 0;
+    }
+    
+    public function getIsSupportAttribute()
+    {
+        return $this->role == 1;
+    }
+    
+    public function getIsClientAttribute()
+    {
+        return $this->role == 2;   
+    }
+    
+    //relacion entre usuarios y proyectos muchos a muchos
+    public function projects()
+    {
+        return $this->belongsToMany('App\Project');
+    }
+
+    public function canTake(Incident $incident)
+    {
+        return ProjectUser::where('user_id', $this->id)
+        //donde el nivel de id del usuario coincide con el nivel de incidencia
+                        ->where('level_id', $incident->level_id)
+                        ->first();
+        
+    }
+
+
+
+    //accesos a proyectos
+     public function getListOfProjectsAttribute()
+    {
+        if ($this->role == 1)
+            return $this->projects;
+        //caso contrario
+        return Project::all();
+    }
+
+
+
+    //funcion scope para listar resultados de busqueda
+
+
+
+    public function scopeBusqueda($query,$select_project_id,$dato="")
+     {
+
+            if($role==0){ 
+            $resultado= $query->where('name','like','%'.$dato.'%')
+                               ->orWhere('email','like', '%'.$dato.'%');
+            }
+            else{
+               
+              $resultado= $query->where("role","=",$role)
+                                  ->Where(function($q) use ($role,$dato)  {
+                                    $q->where('name','like','%'.$dato.'%')
+                                      ->orWhere('email','like', '%'.$dato.'%');       
+                                   });
+
+             }                     
+        
+        return  $resultado;
+     }
 }
