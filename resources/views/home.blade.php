1c43e6336e3b43ad9facfddc15253ab4a902571c
Orixur <agustin.gimenez1996@gmail.com>
2018-09-17 17:06:23 -0300

    Home fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,33 @@
 
 @section('content')
 <div class="pann">
-    <div class="col-lg-12">
-        <div class="panel panel-default">
-            <div class="panel-heading">Dashboard</div>
-            <div class="panel-body">
-                You are logged in!
-            </div>
-        </div>
-    </div>
+	<div class="col-lg-12">
+		<div class="panel panel-default">
+			<div class="panel-heading">Dashboard</div>
+			<table class="table table-striped table-bordered">
+		    <thead>
+		      <tr>
+		        <th>Codigo</th>
+		        <th>Categoria</th>
+		        <th>Severidad</th>
+		        <th>Estado</th>
+		        <th>Fecha de creacion</th>
+		        <th>Titulo</th>
+		      </tr>
+		    </thead>
+		    <tbody>
+		      <tr>
+		      	<td>Cod</td>
+		      	<td>Cat</td>
+		      	<td>Sev</td>
+		      	<td>Est</td>
+		      	<td>Fec</td>
+		      	<td>Tit</td>
+		      </tr>
+		    </tbody>
+		  </table>
+		</div>
+		<!-- /.panel-body -->
+	</div>
 </div>
 @endsection
