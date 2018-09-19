db751da62ac5c734dcfcc4bade429b09fcf9ce80
markzelo <markzelo28@gmail.com>
2018-09-19 12:06:05 -0300

    login views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+
+<div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
@@ -61,8 +59,6 @@
                         </div>
                     </form>
                 </div>
-            </div>
-        </div>
-    </div>
 </div>
+ 
 @endsection
