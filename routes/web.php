09789b55958e4915693ad8b1587516e3969d145c
markzelo <markzelo28@gmail.com>
2018-09-19 10:35:21 -0300

    permisos de admin vistas

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,52 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 Route::get('/seleccionar/proyecto/{id}', 'HomeController@selectProject');
 //-------------------------------------------------------------------
+
+
+
+//----------permisos de admin
+Route::group(['middleware' => 'auth',"namespace"=>"Admin"], function () {
+
+	 // derechos sobre usuarios----------------------------------
+    Route::get('/usuarios', 'UserController@index');
+    Route::post('/usuarios', 'UserController@store');
+    
+    Route::get('/usuario/{id}', 'UserController@edit');
+    Route::post('/usuario/{id}', 'UserController@update');
+	
+	//registrar-----------------------------------------------
+	Route::post('/usuario/{id}/eliminar', 'UserController@delete');
+
+
+	Route::get('/proyectos', 'ProjectController@index');
+	Route::post('/proyectos', 'ProjectController@store');
+	
+    
+    Route::get('/proyecto/{id}', 'ProjectController@edit');
+    Route::post('/proyecto/{id}', 'ProjectController@update');
+
+    //admin los edita--------------------------------------------------------
+    Route::get('/proyecto/{id}/eliminar', 'ProjectController@delete');
+    Route::get('/proyecto/{id}/restaurar', 'ProjectController@restore');
+    
+
+   //categorias--------------------------------------------------------
+    Route::post('/categorias', 'CategoryController@store');
+     Route::post('/categoria/editar', 'CategoryController@update');
+     Route::get('/categoria/{id}/eliminar', 'CategoryController@delete');
+
+    //niveles-------------------------------------------------------------
+    Route::post('/niveles', 'LevelController@store');
+    Route::post('/nivel/editar', 'LevelController@update');
+    Route::get('/nivel/{id}/eliminar', 'LevelController@delete');
+
+    // Project-User-------------------------------------------
+    Route::post('/proyecto-usuario', 'ProjectUserController@store');
+    Route::get('/proyecto-usuario/{id}/eliminar', 'ProjectUserController@delete');
+
+
+
+
+	Route::get('/config', 'ConfigController@index');
+    
+});
