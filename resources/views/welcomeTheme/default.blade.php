358d17e65cece20f0455d6c185294cfaa45bbc96
Orixur <agustin.gimenez1996@gmail.com>
2018-09-21 00:10:16 -0300

    Welcome add

---
+++ resources/views/welcomeTheme/default.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	  <title>SCIT</title>
+	  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+	  <meta content="" name="keywords">
+	  <meta content="" name="description">
+
+	  <!-- Google Fonts -->
+	  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
+
+	  <!-- Bootstrap CSS File -->
+	  <link href="{!! asset('welcomeTheme/lib/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
+
+	  <!-- Libraries CSS Files -->
+	  <link href="!! asset('welcomeTheme/lib/nivo-slider/css/nivo-slider.css') !!}" rel="stylesheet">
+	  <link href="!! asset('welcomeTheme/lib/owlcarousel/owl.carousel.css') !!}" rel="stylesheet">
+	  <link href="{!! asset('welcomeTheme/lib/owlcarousel/owl.transitions.css') !!}" rel="stylesheet">
+	  <link href="{!! asset('welcomeTheme/lib/font-awesome/css/font-awesome.min.css') !!}" rel="stylesheet">
+	  <link href="{!! asset('welcomeTheme/lib/animate/animate.min.css') !!}" rel="stylesheet">
+	  <link href="{!! asset('welcomeTheme/lib/venobox/venobox.css') !!}" rel="stylesheet">
+
+	  <!-- Nivo Slider Theme -->
+	  <link href="{!! asset('welcomeTheme/css/nivo-slider-theme.css') !!}" rel="stylesheet">
+
+	  <!-- Main Stylesheet File -->
+	  <link href="{!! asset('welcomeTheme/css/style.css') !!}" rel="stylesheet">
+
+	  <!-- Responsive Stylesheet File -->
+	  <link href="{!! asset('welcomeTheme/css/responsive.css') !!}" rel="stylesheet">
+</head>
+<body>
+	@include('welcomeTheme.header')
+</body>
+</html>
