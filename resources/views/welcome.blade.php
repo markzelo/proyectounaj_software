3bfd346719265043c3b30bdbc1d553238583a916
Agustin <agustin.gimenez1996@gmail.com>
2018-09-25 00:32:53 -0300

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,1 @@
-<<<<<<< HEAD
-@extends('layouts.app')
-
-
-
-@section('content')
-<div class="panel panel-primary">
-    <div class="panel-heading">arribo - scit unaj</div>
-    <div class="panel-body text-center">
-        
-<!-- igresar slide carrosel de boss -->
-
-        <img src="/images/welcome.png" alt="Sistema de gestión de incidencias" class="img-responsive">
-    </div>
-</div>
-@endsection
-=======
 @extends('welcomeTheme.default')
->>>>>>> rama_agustin
