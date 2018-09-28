c4d10c4c154cad877ce87d1e191f8469440e087a
markzelo <markzelo28@gmail.com>
2018-09-27 22:22:14 -0300

    script nuevo

--- resources/views/theme/default.blade.php
+++ resources/views/theme/default.blade.php
@@ -125,6 +125,36 @@
 
 </body>
 
+<!-- mi radio -->
+<script type="text/javascript" src="http://www.netyco.com:82/radio-x-internet/player/player.js"></script>
+<div id="radioload">Tu radio en Internet <a alt=" radio por internet" href="">Scit corporacion</a>
+</div>
+<script type="text/javascript">
+var params={
+// 'url':'http://streaming.latina101.com.ar:8080/RadioLatina?hash/;',
+'url':'http://198.105.216.204/proxy/mcradio?mp=/;',
+
+'lang':'es',
+'codec':'mp3',
+'volume':100,
+'autoplay':false,
+'buffering':10,
+'title':'mi%20radio',
+'welcome':'solo escucha',
+'bgcolor':'#FFFFFF',
+'winmedia':false,
+'realplayer':false,
+'quicktime':false,
+'html5':true,
+'celulares':false,
+'skin':'mcclean',
+'width':180,
+'height':60
+};
+Netyco.insert(params);
+</script>
+<!--  -->
+
 
 
 </html>
