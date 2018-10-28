756bc95998048a8cdbe9bdedae24ffbb5179e7bb
fernandovargasvarela <fernandovargasvarela@gmail.com>
2018-10-27 21:36:32 -0300

    cambios en visat pdf

--- resources/views/PdfDemo.blade.php
+++ resources/views/PdfDemo.blade.php
@@ -5,9 +5,9 @@
 
 
 
-<div class="pann"></div>
+
 <div class="panel panel-primary">
-	<div class="panel-heading">PDF</div>
+	<div class="panel-heading">Base de Conocimiento</div>
 	<div class="panel-body text-left">
 		<div class="row">
 			<div class="col-md-offset-2 col-md-8">
@@ -28,28 +28,36 @@
 
 				<form action="/PdfDemo/HtmlToPDF" method="POST">
 					{{ csrf_field() }}
-					<table cellspacing="3" cellpadding="5" width="100%">
-						@if (! auth()->user()->is_client)
-						<tr>
+					<div class="panel panel-info">
+  						<div class="panel-heading">
+    						<h3 class="panel-title text-center">Crear Instuctivo</h3>
+  						</div>
+  						
+						<table cellspacing="3" cellpadding="5" width="100%">
+							@if (! auth()->user()->is_client)
 
-							<label>Título</label>
 							<p>
-								<input type="text" name="title" class="form-control">
-							</p>							
-														
-						</tr>
-						<tr>
-							<p>
-								<textarea name="editor1" id="editor1" >{{ old('editor1') }}</textarea>
+								<tr>
+								<p>
+									<label>Título</label>
 								
-								<script>
-		        					$editor = CKEDITOR.replace ("editor1");
-		        					CKFinder.setupCKEditor( $editor, '/ckfinder/' );
-		        				</script>
-
+									<input type="text" name="title" class="form-control">
+								</p>							
+														
+							</tr>
 							</p>
-						</tr>
-						<tr>
+							<tr>
+								<p>
+									<textarea name="editor1" id="editor1" >{{ old('editor1') }}</textarea>
+								
+									<script>
+		        						$editor = CKEDITOR.replace ("editor1");
+		        						CKFinder.setupCKEditor( $editor, '/ckfinder/' );
+		        					</script>
+
+								</p>
+							</tr>
+							<tr>
 							
 								<div class="form-group">
 
@@ -57,30 +65,33 @@
 
 								</div>										
 							
-						</tr>
-						@endif 
-						<tr>
-							<p>
-								<label>Buscar instructivo en formato pdf</label>
-							</p>
-							<div >
-								<p>
-									<input class="form-control" list="titles" name="select" id="select">
-									<datalist id="titles">
-	  								@foreach($pdf_demos as $pdf)
-									 	<option>{{ $pdf->title }}</option>
-									@endforeach
-									</datalist>
-								</p>
-							</div>
-						</tr>
-						<tr>
-							<div class="form-group">
+							</tr>
+							@endif 
+							<div class="panel-heading">
+    							<h3 class="panel-title text-center">Buscar Instructivo</h3>
+  							</div>
+  							<label>Título</label>
+							<tr>
+								
+								<div >
+									<p>
+										<input class="form-control" list="titles" name="select" id="select">
+										<datalist id="titles">
+	  										@foreach($pdf_demos as $pdf)
+									 		<option>{{ $pdf->title }}</option>
+											@endforeach
+										</datalist>
+									</p>
+								</div>
+							</tr>
+							<tr>
+								<div class="form-group">
 
 									<input type="submit" class="btn btn-primary" name="action" value="Ver PDF" />							
-							</div>
-						</tr>
-					</table>
+								</div>
+							</tr>
+						</table>
+					</div>
 				</form>
 			</div>
 
